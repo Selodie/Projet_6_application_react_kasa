96513df972d9b0d7c86c390572e746b812291bc9
Soufflet Elodie <soufflet.elodi@gmail.com>
2024-03-21 18:30:21 +0100

    main / création des pages de l'application. Ajout du fichier json dans le projet. Création du composant pour le header. Installation du sass

---
+++ kasa/src/components/header.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { NavLink } from 'react-router-dom';
+import "../App.sass";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <div>
+                    <img src="./assets/logoKasaDesktop.png" alt="logo de Kasa, la premier a est représenté par une maison"></img>
+                </div>
+                <div className="nav">
+                    <nav>
+                        <ul>
+                            <li className="liHeader">
+                            <NavLink to="/" className="navLink">Accueil</NavLink> 
+                            </li>
+                            <li className="liHeader">
+                            <NavLink to="/about" className="navLink">A propos</NavLink> 
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
+
--- kasa/src/main.jsx
+++ kasa/src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.sass";
+import App from "./pages/home";
+import ErrorPage from "./pages/error404";
+import Roots from "./pages/about";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/home",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <Roots />,
+  },
+  
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
